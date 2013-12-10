67ba2a805b889596a405a39c60141c6e2d3c1959
Ómar Kjartan Yasin <omarkj@gmail.com>
2013-12-09 17:00:09 -0800

    Move some helper functions to utils.

--- src/hstub_upgrade_middleware.erl
+++ src/hstub_upgrade_middleware.erl
@@ -4,11 +4,7 @@
 -export([execute/2]).
 
 execute(Req, Env) ->
-    {ConnectionTokens,Req1} = case cowboy_req:parse_header(<<"connection">>, Req) of
-        {ok, L, Req0} when is_list(L) -> {L, Req0};
-        {ok, Term, Req0} -> {[Term], Req0};
-        {undefined, undefined, Req0} -> {[], Req0}
-    end,
+    {ConnectionTokens,Req1} = hstub_utils:parse_header(<<"connection">>, Req),
     case lists:member(<<"upgrade">>, ConnectionTokens) of
         false ->
             {ok, Req1, Env};
