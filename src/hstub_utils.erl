d616a81fa4226cb3a4f12d95736875c70bee7e10
Ómar Kjartan Yasin <omarkj@gmail.com>
2014-01-06 13:13:05 -0800

    Make sure that `parse_header/2` always returns a list of header values.

--- src/hstub_utils.erl
+++ src/hstub_utils.erl
@@ -11,6 +11,7 @@ parse_header(Key, Req) ->
     case cowboy_req:parse_header(Key, Req) of
         {ok, L, Req0} when is_list(L) -> {L, Req0};
         {ok, Term, Req0} -> {[Term], Req0};
+        {undefined, L, Req0} when is_list(L) -> {L, Req0};
         {undefined, _, Req0} -> {[], Req0}
     end.
 
