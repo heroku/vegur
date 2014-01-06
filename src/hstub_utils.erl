8aadaa4c78dc04b2fe550943777c29a581a632b2
Ómar Kjartan Yasin <omarkj@gmail.com>
2014-01-06 14:59:44 -0800

    Match all possible cases from cowboy.

--- src/hstub_utils.erl
+++ src/hstub_utils.erl
@@ -23,8 +23,8 @@ parse_header(Key, Req) ->
     case cowboy_req:parse_header(Key, Req) of
         {ok, L, Req0} when is_list(L) -> {L, Req0};
         {ok, Term, Req0} -> {[Term], Req0};
-        {undefined, L, Req0} when is_list(L) -> {L, Req0};
-        {undefined, _, Req0} -> {[], Req0}
+        {undefined, undefined, Req0} -> {[], Req0};
+        {undefined, L, Req0} -> {L, Req0}
     end.
 
 -spec add_or_append_header(Key, Value, Headers, Req) ->
