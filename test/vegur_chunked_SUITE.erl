f8e5a4cd2a6941598b6e60b4ba1095e4c26ed4e7
omarkj <omarkj@gmail.com>
2014-05-26 20:21:15 -0700

    Add test for reading chunk byte-by-byte to emulate package boundaries.

--- test/vegur_chunked_SUITE.erl
+++ test/vegur_chunked_SUITE.erl
@@ -2,7 +2,7 @@
 -include_lib("common_test/include/ct.hrl").
 -compile(export_all).
 
-all() -> [bad_length, html, short_msg, stream, trailers].
+all() -> [bad_length, html, short_msg, stream, trailers, boundary_chunk].
 
 init_per_testcase(trailers, _) ->
     {skip, "Trailers not supported"};
@@ -105,3 +105,34 @@ trailers(_) ->
     "Expires: Sat, 27 Mar 2004 21:12:00 GMT\r\n">>,
     {done, Buf, <<>>} = vegur_chunked:all_chunks(String, <<"Expires">>),
     String = iolist_to_binary(Buf).
+
+boundary_chunk(_) ->
+    Chunks = <<""
+    "c\r\n"
+    "<h1>go!</h1>\r\n"
+    "1b\r\n"
+    "<h1>first chunk loaded</h1>\r\n"
+    "2a\r\n"
+    "<h1>second chunk loaded and displayed</h1>\r\n"
+    "29\r\n"
+    "<h1>third chunk loaded and displayed</h1>\r\n"
+    "0\r\n">>,
+    done = parse_chunked(Chunks, undefined).
+
+parse_chunked(<<>>, _State) ->
+    done;
+parse_chunked(<<B:1/binary, Rest/binary>>, State) ->
+    parse(B, Rest, State).
+
+
+parse(What, Rest, State) ->
+    case vegur_chunked:stream_chunk(What, State) of
+        {done, _, _} ->
+            parse_chunked(Rest, State);
+        {error, _Reason} = Res ->
+            Res;
+        {chunk, _Chunk, MoreBuffer} ->
+            parse_chunked(<<Rest/binary, MoreBuffer/binary>>, undefined);
+        {more, _, _, State1} ->
+            parse_chunked(Rest, State1)
+    end.
