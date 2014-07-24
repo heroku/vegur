8207b870baf1aa1fa1e5e1925de8184f0dc22d27
Geoff Cant <nem@erlang.geek.nz>
2014-07-24 16:46:09 -0700

    Test blank phrases, simplify suite code.

--- test/vegur_client_SUITE.erl
+++ test/vegur_client_SUITE.erl
@@ -17,7 +17,9 @@
 
 all() ->
     [missing_reason_phrase
-    ,deliberate_reason_phrase].
+    ,deliberate_reason_phrase
+    ,blank_reason_phrase
+    ].
 
 %%%%%%%%%%%%%%%%%%%%%%
 %%% Setup/Teardown %%%
@@ -34,22 +36,30 @@ end_per_suite(Config) ->
     Config.
 
 %% Runs before the test case. Runs in the same process.
-init_per_testcase(deliberate_reason_phrase, Config) ->
-    ranch:start_listener(deliberate_reason_phrase,
-                         10,
-                         ranch_tcp,
-                         [{port, 9988}],
-                         ?MODULE,
-                         [{handler, fun deliberate_reason_phrase_resp/2}]),
-    [{dyno_port, 9988} | Config];
 init_per_testcase(missing_reason_phrase, Config) ->
     ranch:start_listener(missing_reason_phrase,
                          10,
                          ranch_tcp,
-                         [{port, 9987}],
+                         [{port, Port = 9987}],
                          ?MODULE,
                          [{handler, fun missing_reason_phrase_resp/2}]),
-    [{dyno_port, 9987} |Config];
+    [{dyno_port, Port} |Config];
+init_per_testcase(deliberate_reason_phrase, Config) ->
+    ranch:start_listener(deliberate_reason_phrase,
+                         10,
+                         ranch_tcp,
+                         [{port, Port = 9988}],
+                         ?MODULE,
+                         [{handler, fun deliberate_reason_phrase_resp/2}]),
+    [{dyno_port, Port} | Config];
+init_per_testcase(blank_reason_phrase, Config) ->
+    ranch:start_listener(deliberate_reason_phrase,
+                         10,
+                         ranch_tcp,
+                         [{port, Port = 9989}],
+                         ?MODULE,
+                         [{handler, fun blank_reason_phrase_resp/2}]),
+    [{dyno_port, Port} | Config];
 init_per_testcase(_CaseName, Config) ->
     Config.
 
@@ -69,16 +79,8 @@ missing_reason_phrase_resp(Socket, Transport) ->
     Transport:close(Socket).
 
 missing_reason_phrase(Config) ->
-    Port = ?config(dyno_port, Config),
-    {ok, C1} = vegur_client:init([]),
-    {ok, C2} = vegur_client:connect(ranch_tcp, {127,0,0,1}, Port, C1),
-    Req = vegur_client:request_to_iolist(<<"GET">>, [], <<>>,
-                                         'HTTP/1.1', <<"test.dyno">>,
-                                         <<"/">>),
-    {ok, C3} = vegur_client:raw_request(Req, C2),
-    {ok, 421, <<"421 Unknown">>, _, C4} = vegur_client:response(C3),
-    vegur_client:close(C4),
-    ok.
+    dyno_req(?config(dyno_port, Config),
+             421, <<"421 Unknown">>).
 
 deliberate_reason_phrase_resp(Socket, Transport) ->
     Transport:send(Socket,
@@ -88,14 +90,28 @@ deliberate_reason_phrase_resp(Socket, Transport) ->
     Transport:close(Socket).
 
 deliberate_reason_phrase(Config) ->
-    Port = ?config(dyno_port, Config),
+    dyno_req(?config(dyno_port, Config),
+             421, <<"421 Deliberate Phrase">>).
+
+blank_reason_phrase_resp(Socket, Transport) ->
+    Transport:send(Socket,
+                   <<"HTTP/1.1 421 \r\n"
+                     "Content-Length: 0\r\n"
+                     "\r\n">>),
+    Transport:close(Socket).
+
+blank_reason_phrase(Config) ->
+    dyno_req(?config(dyno_port, Config),
+             421, <<"421 ">>).
+
+dyno_req(Port, Code, Status) ->
     {ok, C1} = vegur_client:init([]),
     {ok, C2} = vegur_client:connect(ranch_tcp, {127,0,0,1}, Port, C1),
     Req = vegur_client:request_to_iolist(<<"GET">>, [], <<>>,
                                          'HTTP/1.1', <<"test.dyno">>,
                                          <<"/">>),
     {ok, C3} = vegur_client:raw_request(Req, C2),
-    {ok, 421, <<"421 Deliberate Phrase">>, _, C4} = vegur_client:response(C3),
+    {ok, Code, Status, _, C4} = vegur_client:response(C3),
     vegur_client:close(C4),
     ok.
 
