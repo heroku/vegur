0804f6d44cde5012509b94d78ec63271d5884ab2
Fred Hebert <mononcqc@ferd.ca>
2015-08-20 14:07:29 -0400

    Copyright notices everywhere as per SFDC policy
    You gotta do what you gotta do.

--- src/vegur_bytepipe.erl
+++ src/vegur_bytepipe.erl
@@ -1,19 +1,41 @@
+%%% Copyright (c) 2013-2015, Heroku Inc <routing-feedback@heroku.com>.
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions are
+%%% met:
+%%%
+%%% * Redistributions of source code must retain the above copyright
+%%%   notice, this list of conditions and the following disclaimer.
+%%%
+%%% * Redistributions in binary form must reproduce the above copyright
+%%%   notice, this list of conditions and the following disclaimer in the
+%%%   documentation and/or other materials provided with the distribution.
+%%%
+%%% * The names of its contributors may not be used to endorse or promote
+%%%   products derived from this software without specific prior written
+%%%   permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%% A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%% OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+%%% LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+%%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%
+%%% @doc
 %%% This module implements a bi-directional pipeline
 %%% to take two different ports and get them to communicate
 %%% together.
 %%%
 %%% Its interface is based on Ranch's transports (which can
-%%% be used by adding -behaviour(ranch_transport). to the module),
-%%% but more specifically requires:
-%%%
-%%% - messages/0
-%%% - recv/3
-%%% - send/2
-%%% - setopts/2
-%%% - close/1
-%%% - controlling_process/2
-%%% ? peername/1
-%%% ? sockname/1
+%%% be used by adding `-behaviour(ranch_transport).' to the module),
+%%% @end
 -module(vegur_bytepipe).
 
 %% API
